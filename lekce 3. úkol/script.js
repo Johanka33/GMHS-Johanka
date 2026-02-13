a0c4417c7820aa6ccf0869418c1d8027a5ac6916
Johanka33 <1228@gmhs.cz>
2026-02-13 10:56:02 +0100

    13.2.2026

---
+++ lekce 3. úkol/script.js
@@ -0,0 +1,18 @@
+alert("Zkouška")
+alert(3)
+
+let userName = "Petr";
+console.log(userName);
+
+userNameL = "Johanka";
+console.log(userNameL);
+
+let ageJ = 18;
+console.log(ageJ);
+let ageP = 16;
+console.log(ageP);
+let soucet = ageJ+ageP;
+console.log(soucet);
+
+let text = userNameL + " a " + userName +" mají "+ soucet +" let.";
+console.log(text);
