d0faa030461afa142a51bb05db3e9d9f9272a48c
Johanka33 <1228@gmhs.cz>
2026-05-15 10:35:03 +0200

    15.5.2026

---
+++ 15.5.2026/scipt.js
@@ -0,0 +1,32 @@
+let pozdrav = "Zdravím";  //Global Scope
+
+
+function sayHello() {  //Deklarace funkce
+let pozdravFunkce = "Pozdrav z funkce"      //Function Scope
+    console.log(pozdravFunkce)
+}
+
+if(true) {
+    let zprava = "Toto je bloková zpráva"
+    console.log(zprava)
+}
+
+
+sayHello();   //Volání funkce
+
+function pozdrav2(name) {
+    console.log("Zdravím" + name);
+}
+
+pozdrav2("Honza")
+
+function calc(num1, num2) {
+    console.log(num1 + num2)
+    console.log(num1 - num2)
+    console.log(num1 * num2)
+    console.log(num1 / num2)
+
+}
+
+
+calc(6,7)
